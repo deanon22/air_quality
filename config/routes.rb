7ab71449ebfa06a2da971acca4e619d564a5c467
Jason Dean <jdean22@gmail.com>
2019-08-31 10:20:09 -0400

    Fixed Root Routes, Removed Index Method code, added link to Homepage in Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+
+  get '/zipcode', to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #get 'home/zipcode'
-  root 'home#zipcode'
-  post "zipcode" => 'home#zipcode'
+  root 'home#index'  
+  post 'zipcode' => 'home#zipcode'
 end
